630d261151383bc8125531524a977df174823610
ga0nii <kgw0124@kakao.com>
2023-04-26 14:50:00 +0900

    project init

---
+++ Crewpass/src/main/java/Capstone/Crewpass/CrewpassApplication.java
@@ -0,0 +1,13 @@
+package Capstone.Crewpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewpassApplication.class, args);
+	}
+
+}
---
+++ Crewpass/src/test/java/Capstone/Crewpass/CrewpassApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.Crewpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
