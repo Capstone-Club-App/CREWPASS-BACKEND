eff3967205079e24ca649495c7a5b60ca2281a81
ga0nii <kgw0124@kakao.com>
2023-05-09 15:15:47 +0900

    Revert "[merge/PR #2-1]"
    This reverts commit 0d42fb8ae36cea362b78cd516088995df1178a4d, reversing
    changes made to 630d261151383bc8125531524a977df174823610.

--- Crewpass/src/main/java/Capstone/Crewpass/CrewpassApplication.java
+++ Crewpass/src/main/java/Capstone/Crewpass/CrewpassApplication.java
@@ -2,9 +2,7 @@ package Capstone.Crewpass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //Auditing이 가능하도록 어노테이션 설정
 @SpringBootApplication
 public class CrewpassApplication {
 
