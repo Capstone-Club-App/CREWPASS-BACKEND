4c3ae20017547f140b8999602fb163f8f672c1d8
ga0nii <kgw0124@kakao.com>
2023-05-14 21:05:15 +0900

    [fix] Crew 로그인 후, crew_id 반환 방식 수정

--- Crewpass/src/main/java/Capstone/Crewpass/entity/Login.java
+++ Crewpass/src/main/java/Capstone/Crewpass/entity/Login.java
@@ -8,10 +8,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Login {
+    private String crew_user_id;
     private String loginId;
     private String password;
 
-    public Login(String loginId, String password) {
+    public Login(String crew_user_id, String loginId, String password) {
+        this.crew_user_id = crew_user_id;
         this.loginId = loginId;
         this.password = password;
     }
