7bc2935ca43eba467b6486b72f94c90fc89ec00b
ga0nii <kgw0124@kakao.com>
2023-05-21 17:42:15 +0900

    [bug] 합격/불합격 email 통보 시, Timeout 오류 해결

--- Crewpass/src/main/java/Capstone/Crewpass/service/NoticeService.java
+++ Crewpass/src/main/java/Capstone/Crewpass/service/NoticeService.java
@@ -10,6 +10,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @Slf4j
@@ -24,31 +25,36 @@ public class NoticeService {
     }
 
     public String notice(String crewName, String[] userId, String msg) {
-        for(int i = 0; i<userId.length ; i++){
+        for(int i=0;i<userId.length;i++) {
+            //" 제거
+            if (i == 0) { //맨 앞 "
+                userId[i] = userId[i].substring(1);
+            } else if (i == userId.length - 1) { //맨 뒤 "
+                userId[i] = userId[i].substring(0, userId[i].length() - 1);
+            }
+
             //userId를 통해 userEmail 추출
             Optional<User> optionalUser = userRepository.findByUserId(userId[i]);
-            //userEmail 주소로 보내고자 하는 내용
-            String sender = "crewpass@crewpass.com";
-            String receiver = optionalUser.get().getUserEmail();
-            String title = "[Crewpass] " + crewName + " 모집 결과 안내";
-            String content= msg;
-            //메시지 전송
-            sendMsg(sender, receiver, title, content);
+            if (optionalUser.isPresent()) {
+                //userEmail 주소로 보내고자 하는 내용
+                String sender = "crewpass@crewpass.com";
+                String receiver = optionalUser.get().getUserEmail();
+                String title = "[Crewpass] " + crewName + " 모집 결과 안내";
+                String content = msg;
+                //메시지 전송
+                try {
+                    MimeMessage message = javaMailSender.createMimeMessage();
+                    MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+                    helper.setFrom(sender);
+                    helper.setTo(receiver);
+                    helper.setSubject(title);
+                    helper.setText(content, true);
+                    javaMailSender.send(message);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
         return "msg 전송 완료";
-    }
-
-    public void sendMsg(String sender, String receiver, String title, String content) {
-        try {
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
-            helper.setFrom(sender);
-            helper.setTo(receiver);
-            helper.setSubject(title);
-            helper.setText(content, true);
-            javaMailSender.send(message);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
