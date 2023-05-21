7d585156a51b4a1beb6ab5225e074cf8ba2c96dd
ga0nii <kgw0124@kakao.com>
2023-05-21 16:33:15 +0900

    [refactor] entity 중 DB 테이블에 관한 파일 분리

--- Crewpass/src/main/java/Capstone/Crewpass/controller/NoticeController.java
+++ Crewpass/src/main/java/Capstone/Crewpass/controller/NoticeController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
