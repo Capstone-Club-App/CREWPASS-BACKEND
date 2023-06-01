398a99a236c372d5e6cae5cbc95aa14e02d06410
MinYeongPark <yingo24655@naver.com>
2023-06-02 01:20:37 +0900

    [refact] 채팅방 리스트 출력 시 채팅방 ID도 반환

--- Crewpass/src/main/java/Capstone/Crewpass/dto/ChatRoomList.java
+++ Crewpass/src/main/java/Capstone/Crewpass/dto/ChatRoomList.java
@@ -2,6 +2,7 @@ package Capstone.Crewpass.dto;
 
 public interface ChatRoomList {
     Integer getCrew_id();
+    Integer getChat_room_id();
     String getCrew_name();
     String getCrew_profile();
     String getTitle();
--- Crewpass/src/main/java/Capstone/Crewpass/repository/QuestionRepository.java
+++ Crewpass/src/main/java/Capstone/Crewpass/repository/QuestionRepository.java
@@ -6,9 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    // Recruitment Id로 찾기
-    // Recruitment : Question = 1 : 1 관계이므로 RecruitmentId로 찾는 것으로 진행함
-    Optional<Question> findByRecruitmentId(Integer recruitmentId);
-
     Optional<Question> findByQuestionId(Integer questionId);
 }
